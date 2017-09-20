100949d70fb0e65a299f6ae3c55752f8568b2824
WaxyMocha <jakubmateusz@poczta.onet.pl>
2017-09-20 22:26:11 +0200

    modified:   GetMic/GetMic.cpp 	modified:   GetMic/GetMic.vcxproj 	modified:   GetMic/GetMic.vcxproj.filters 	new file:   GetMic/WAV.cpp 	new file:   GetMic/WAV.h 	modified:   GetMic/stdafx.h

--- GetMic/GetMic.cpp
+++ GetMic/GetMic.cpp
@@ -2,9 +2,9 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include "portaudio.h"
 #include "..\WAV2CSV.h"
+#include "WAV.h"
 
 using namespace std;
 
@@ -25,17 +25,12 @@ struct paTestData
 	SAMPLE      *recordedSamples;
 };
 
-int save_WAV(string filename, float *samples, int numOfSamples);
-void create_wav_header(fstream &file, int &pos, int numOfSamples);
-void flip_Endian(char *in, char *out, int lenght);
-void int2char(int in, char *out, int lenght);
-void float2char(float in, char *out, int lenght);
 
 static int recordCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData);
 
 int main(int argc, char** argv)
 {
-	PaStreamParameters  inputParameters, outputParameters;
+	PaStreamParameters  inputParameters;
 	PaStream*           stream;
 	PaError             err = paNoError;
 	paTestData          data;
@@ -136,159 +131,6 @@ int main(int argc, char** argv)
 	return err;
 }
 
-int save_WAV(string filename, float *samples, int numOfSamples)
-{
-	fstream file;
-	file.open(filename, ios::binary | ios::out);
-
-	if (!file.good())
-	{
-		cout << "Error while loading file" << endl;
-		file.close();
-		return 1;
-	}
-
-	char buff[16] = { 0 };
-	int pos = 0;
-
-	create_wav_header(file, pos, numOfSamples);
-
-	for (int i = 0; i < numOfSamples; i++)
-	{
-		float2char(samples[i], buff, 4);
-		flip_Endian(buff, buff, 4);
-
-		file.seekg(pos);
-		file.write(buff, 4);
-		pos += 4;
-	}
-
-	file.close();
-
-	return 0;
-}
-
-void create_wav_header(fstream &file, int &pos, int numOfSamples)
-{
-	char buff[16] = { 0 };
-
-	file.write("RIFF", 4);
-	pos += 4;
-
-	int2char(((numOfSamples * 4) + 44), buff, 4);//File size
-	flip_Endian(buff, buff, 4);
-	file.seekg(pos);
-	file.write(buff, 4);
-	pos += 4;
-
-	file.seekg(pos);
-	file.write("WAVE", 4);
-	pos += 4;
-
-	file.seekg(pos);
-	file.write("fmt ", 4);
-	pos += 4;
-
-	int2char(0x10, buff, 4);
-	flip_Endian(buff, buff, 4);
-	file.seekg(pos);
-	file.write(buff, 4);
-	pos += 4;
-
-	int2char(0x3, buff, 2);
-	flip_Endian(buff, buff, 2);
-	file.seekg(pos);
-	file.write(buff, 2);
-	pos += 2;
-
-	int2char(0x1, buff, 2);//Number of chanels
-	flip_Endian(buff, buff, 2);
-	file.seekg(pos);
-	file.write(buff, 2);
-	pos += 2;
-
-	int2char(0x3E80, buff, 4);//Sample rate
-	flip_Endian(buff, buff, 4);
-	file.seekg(pos);
-	file.write(buff, 4);
-	pos += 4;
-
-
-	int2char(0xFA00, buff, 4);//(Sample Rate * BitsPerSample * Channels) / 8.
-	flip_Endian(buff, buff, 4);
-	file.seekg(pos);
-	file.write(buff, 4);
-	pos += 4;
-
-	int2char(0x4, buff, 2);//(BitsPerSample * Channels) / 8 = (32 * 1)/8 = 4
-	flip_Endian(buff, buff, 2);
-	file.seekg(pos);
-	file.write(buff, 2);
-	pos += 2;
-
-	int2char(0x20, buff, 2);//Bits per sample
-	flip_Endian(buff, buff, 2);
-	file.seekg(pos);
-	file.write(buff, 2);
-	pos += 2;
-
-	file.seekg(pos);
-	file.write("data", 4);
-	pos += 4;
-
-	int2char((numOfSamples * 4), buff, 4);
-	flip_Endian(buff, buff, 4);
-	file.seekg(pos);
-	file.write(buff, 4);
-	pos += 4;
-}
-
-void flip_Endian(char *in, char *out, int lenght)
-{
-	char *tmp = new char[lenght];
-
-	int j = 0;
-	for (int i = lenght - 1; i >= 0; i--)
-	{
-		tmp[i] = in[j];
-
-		j++;
-	}
-	for (int i = lenght - 1; i >= 0; i--)
-	{
-		out[i] = tmp[i];
-	}
-
-	return;
-}
-
-void int2char(int in, char *out, int lenght)
-{
-	int shift = (lenght * 8) - 8;
-
-	for (int i = 0; i < lenght; i++)
-	{
-		out[i] = (in >> shift) & 0xFF;
-		shift -= 8;
-	}
-	return;
-}
-
-void float2char(float in, char *out, int lenght)
-{
-	int tmp = 0;
-
-	memcpy(&tmp, &in, sizeof(tmp));
-
-	int shift = (lenght * 8) - 8;
-
-	for (int i = 0; i < lenght; i++)
-	{
-		out[i] = (tmp >> shift) & 0xFF;
-		shift -= 8;
-	}
-	return;
-}
 
 static int recordCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData)
 {
--- GetMic/stdafx.h
+++ GetMic/stdafx.h
@@ -11,6 +11,7 @@
 #include <tchar.h>
 
 #include <fstream>
+#include <iostream>
 
 
 // TODO: W tym miejscu odwołaj się do dodatkowych nagłówków wymaganych przez program
